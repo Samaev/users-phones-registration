ac128d6d559d41698f1fab36320b5d9a40ba7b64
Andrey Samaev <andreysamaev@gmail.com>
2024-03-06 13:53:44 +0200

    update sms service

--- app/Services/SMSService.php
+++ app/Services/SMSService.php
@@ -9,6 +9,8 @@ class SMSService
     public function sendRegistrationConfirmation($phoneNumber, $message)
     {
         $account_sid = "ACa600d87e517168502c90908bdb0254a0";
+
+//    TODO: this token is being revoked every time I publish code to GitHUB by security rules
         $auth_token = "182be7eedfceb615b1ac890f8b6c2f09";
         $twilio_number = "+15169906171";
 
